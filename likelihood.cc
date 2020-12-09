ac58876c6199de26f6aa4049d09cb317e735ef1f
repl.it user <replituser@example.com>
2020-12-09 15:58:08 +0000

    starting 2e

--- likelihood.cc
+++ likelihood.cc
@@ -10,14 +10,24 @@ double poisson(double mu, int k) {
   double p = 0.0;
   p = pow(mu, k) * exp(-1.0 * mu) / tgamma(k + 1.0);
   return p;
-}
+}  // end poisson
+
 double prob(std::vector<int> daten, double mu) {
   double p = 1.0;
   for (unsigned int i = 0; i < daten.size(); ++i) {
     p = p * poisson(mu, daten[i]);
   }
   return p;
-}
+}  // end prob
+
+double high_prob (std::vector<int> daten) {
+  double hp = 1.0;
+  for (unsigned int i = 0; i < daten.size(); ++i) {
+    hp = hp * poisson(daten[i], daten[i]);
+  }
+  return hp;
+}  // end high_prob
+
 int main() {
     int n_i;
     double a_sum = 0.0, a2_sum = 0.0;
@@ -51,34 +61,38 @@ int main() {
     // gnuplot: plot "likelihood.txt" using 1:2 with lines
 
     // exercise 2c
-    double l, ll;
+    double log_prob;
     ofstream ofi2("nll.txt");
     for(int j = 0; j <= 60; j++){
       mu = 0.0 + j / 10.0;
-      l = prob(daten, mu);
-      ll = -2.0 * log(l);
-      ofi2 << mu << " " << ll << endl;
+      log_prob = -2.0  * log(prob(daten, mu));
+      ofi2 << mu << " " << log_prob << endl;
     }
     ofi2.close();
     // gnuplot: plot "nll.txt" using 1:2 with lines 
 
     // exercise 2d
     ofstream ofi3("deltanll.txt");
-    double lmin_mu = -2.0 * log(min_mu);
+    double log_min_mu = -2.0 * log(min_mu);
     for(int j = 0; j <= 100; j++){
       mu = 2.9 + j / 100.0;
-      l = prob(daten, mu);
-      ll = -2.0 * log(l);
-      ofi3 << mu << " " << ll - lmin_mu << endl;
+      log_prob = -2.0  * log(prob(daten, mu));
+      ofi3 << mu << " " << log_prob - log_min_mu << endl;
     }
     ofi3.close();
     // gnuplot: plot [2.95:3.3][] "deltanll.txt" using 1:2 with lines, 1.0
     cout << "mean = " << mean << " sigma = " << sigma << endl;
     
     // exercise 2e
-    // Our job for Wednesday
-
-}
+    // Our job for Wednesday result should be 233.942
+    double ratio, z;
+    int ndof = 233;
+    ratio = prob(daten, mu) / high_prob (daten);
+    cout << "Ratio = " << ratio << 
+      ", log ratio = " << -2.0* log(ratio) << endl;
+    z = (-2.0 * log(ratio) - ndof) / sqrt(2.0 * ndof);
+    cout << "relative deviation = " << z << endl;
+}  // end main
 /*
 // Sheet 2, Exercise 2a
   while (fin >> a) {
